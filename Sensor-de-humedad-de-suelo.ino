4e3366fa5a1c585ab7fc47c7248069455e756412
proyectoTEOS <proyectoteos@gmail.com>
2017-04-03 01:41:05 -0500

    Files for the project
    Files for the project "Humedad de Suelo con Aduino"

---
+++ Sensor-de-humedad-de-suelo.ino
@@ -0,0 +1,23 @@
+const int sensorPinT = A0;
+
+int valT;
+int finalvalT;
+
+void setup(){
+  Serial.begin(9600);
+}
+void loop(){
+  valT = map(analogRead(sensorPinT), 0, 1023, 100, 0);
+  finalvalT = constrain (valT, 0, 100);
+  Serial.print("Hay un ");
+  Serial.println(finalvalT);
+  Serial.println("% de humedad"); 
+  if((finalvalT >=0) and (finalvalT <=33)){
+    Serial.println("Suelo seco");
+  }else if((finalvalT >=34) and (finalvalT <= 66)){
+    Serial.println("Suelo humedo");
+  }else{
+    Serial.println("Suelo mojado");
+  }
+  delay(1000); 
+}
