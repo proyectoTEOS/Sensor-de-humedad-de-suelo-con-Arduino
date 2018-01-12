e237c984185322cb20fa39cd74201e4f40af3bf0
TEOS <proyectoteos@gmail.com>
2018-01-12 17:40:37 -0600

    Code update
    Improvement of the sensor reading

--- Sensor-de-humedad-de-suelo.ino
+++ Sensor-de-humedad-de-suelo.ino
@@ -13,20 +13,19 @@
 const int sensorPinT = A0;
 
 int valT;
-int finalvalT;
 
 void setup() {
   Serial.begin(9600);
 }
 void loop() {
   valT = map(analogRead(sensorPinT), 0, 1023, 100, 0);
-  finalvalT = constrain (valT, 0, 100);
+  valT = constrain (valT, 0, 100);
   Serial.print("Hay un ");
-  Serial.println(finalvalT);
-  Serial.println("% de humedad");
-  if ((finalvalT >= 0) and (finalvalT <= 33)) {
+  Serial.print(valT);
+  Serial.print("% de humedad, ");
+  if ((valT >= 0) and (valT <= 33)) {
     Serial.println("Suelo seco");
-  } else if ((finalvalT >= 34) and (finalvalT <= 66)) {
+  } else if ((valT >= 34) and (valT <= 66)) {
     Serial.println("Suelo humedo");
   } else {
     Serial.println("Suelo mojado");
